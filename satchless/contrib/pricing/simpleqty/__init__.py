4e47e98ef62445a04777690c67c4284d467baa54
Andrés Moya <andres.moya@kaleidos.net>
2012-04-18 19:50:15 +0200

    Protect simpleqty pricing handler to work with products that doesn't have the mixin

--- satchless/contrib/pricing/simpleqty/__init__.py
+++ satchless/contrib/pricing/simpleqty/__init__.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.db.models import Min, Max
 
 from ....pricing import Price, PriceRange, PricingHandler
@@ -15,32 +16,42 @@ class SimpleQtyPricingHandler(PricingHandler):
         return quantity
 
     def get_variant_price(self, variant, currency, quantity=1, **kwargs):
-        if variant.product.qty_mode == 'product':
-            quantity = self._get_variants_count(variant, currency, quantity, **kwargs)
-        price_overrides = (variant.product.get_qty_price_overrides().filter(min_qty__lte=quantity)
-                                                                    .order_by('-min_qty'))
-        try:
-            price = price_overrides[0].price
-        except IndexError:
-            price = variant.product.price
-        if variant.price_offset:
-            price = price + variant.price_offset
-        return Price(net=price, gross=price, currency=currency)
+        if hasattr(variant.product, 'qty_mode'):
+            if variant.product.qty_mode == 'product':
+                quantity = self._get_variants_count(variant, currency, quantity, **kwargs)
+            price_overrides = (variant.product.get_qty_price_overrides().filter(min_qty__lte=quantity)
+                                                                        .order_by('-min_qty'))
+            try:
+                price = price_overrides[0].price
+            except IndexError:
+                price = variant.product.price
+            if variant.price_offset:
+                price = price + variant.price_offset
+            return Price(net=price, gross=price, currency=currency)
+        else:
+            price = kwargs.get('price')
+            if not price:
+                price = Price(net=Decimal(0), gross=Decimal(0), currency=currency)
+            return price
 
     def get_product_price_range(self, product, currency, **kwargs):
-        price_overrides = (product.get_qty_price_overrides().filter(min_qty__lte=1)
-                                                            .order_by('-min_qty'))
-        try:
-            price = price_overrides[0].price
-        except IndexError:
-            price = product.price
-        max_price = min_price = price
-        min_offset = product.variants.all().aggregate(Min('price_offset'))['price_offset__min']
-        max_offset = product.variants.all().aggregate(Max('price_offset'))['price_offset__max']
-        if max_offset is not None:
-            max_price = max(price, price + max_offset)
-        if min_offset is not None:
-            min_price = min(price, price + min_offset)
+        if hasattr(product, 'get_qty_price_overrides'):
+            price_overrides = (product.get_qty_price_overrides().filter(min_qty__lte=1)
+                                                                .order_by('-min_qty'))
+            try:
+                price = price_overrides[0].price
+            except IndexError:
+                price = product.price
+            max_price = min_price = price
+            min_offset = product.variants.all().aggregate(Min('price_offset'))['price_offset__min']
+            max_offset = product.variants.all().aggregate(Max('price_offset'))['price_offset__max']
+            if max_offset is not None:
+                max_price = max(price, price + max_offset)
+            if min_offset is not None:
+                min_price = min(price, price + min_offset)
+        else:
+            min_price = Decimal(0)
+            max_price = Decimal(0)
         min_price = Price(net=min_price, gross=min_price, currency=currency)
         max_price = Price(net=max_price, gross=max_price, currency=currency)
         return PriceRange(min_price=min_price, max_price=max_price)
